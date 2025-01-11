7165155d2c1531b3c0f4a21a2f832e5cdacd1528
stepaxvii <153988379+stepaxvii@users.noreply.github.com>
2025-01-11 19:56:45 +0300

    v/1.6

--- yandex/yandex_primary_collection.py
+++ yandex/yandex_primary_collection.py
@@ -1,3 +1,4 @@
+import logging
 from selenium.webdriver import Firefox, FirefoxOptions
 from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.service import Service
@@ -25,6 +26,10 @@ from constants import (
 )
 from utils.urls import process_url_yandex
 
+# Настройка логирования
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
 load_dotenv()
 
 DRIVER_PATH = getenv('DRIVER_PATH', default='')
@@ -35,22 +40,35 @@ def ya_prim_coll(original_url):
     Функция для первичного сбора отзывов ресторана
     и заполнения БД данными о ресторане и отзывами.
     """
+    logger.info(f"Начинаем сбор данных с URL: {original_url}")
+
     options = FirefoxOptions()
     options.add_argument('--headless')
     service = Service(DRIVER_PATH)
-    driver = Firefox(service=service, options=options)
-    actions = ActionChains(driver)
+
+    # Инициализация драйвера Firefox
+    try:
+        logger.info("Запускаем Firefox WebDriver...")
+        driver = Firefox(service=service, options=options)
+        actions = ActionChains(driver)
+        logger.info("WebDriver успешно запущен.")
+    except Exception as e:
+        logger.error(f"Ошибка при запуске WebDriver: {e}")
+        return
 
     # Переходим на url юзера
+    logger.info(f"Переходим по URL: {original_url}")
     driver.get(original_url)
     sleep(2)
 
     # Извлекаем полный url-адрес для дальнейшей работы
     full_org_url = driver.current_url
+    logger.info(f"Полный URL компании: {full_org_url}")
 
     org_url, reviews_url = process_url_yandex(full_org_url)
 
     # Переходим на страницу компании и ждём полной загрузки
+    logger.info(f"Переходим на страницу компании: {org_url}")
     driver.get(org_url)
     sleep(5)
 
@@ -58,40 +76,51 @@ def ya_prim_coll(original_url):
     try:
         org_name_element = driver.find_element(By.CSS_SELECTOR, ORG_NAME_BLOCK)
         org_name = org_name_element.text.strip()
+        logger.info(f"Название организации: {org_name}")
     except NoSuchElementException:
         org_name = None
-        print("Не удалось найти название организации")
+        logger.error("Не удалось найти название организации")
 
     # Получаем полный адрес с обработкой ошибок
     try:
         address_element = driver.find_element(By.CLASS_NAME, ORG_ADDRESS_BLOCK)
         full_address = address_element.text.strip()
+        logger.info(f"Адрес организации: {full_address}")
     except NoSuchElementException:
         full_address = None
-        print("Не удалось найти полный адрес")
+        logger.error("Не удалось найти полный адрес")
 
     # Проверяем, что данные получены перед сохранением в базу данных
     if org_name is not None:
         # Формирование общей информации о ресторане
         restaurant_data = (org_name, org_url, full_address)
         try:
+            logger.info("Добавляем ресторан в базу данных...")
             create_restaurant(data=restaurant_data)
+            logger.info("Ресторан успешно добавлен в базу данных.")
         except Exception as e:
-            print(f"Ошибка при добавлении ресторана в базу данных: {e}")
+            logger.error(f"Ошибка при добавлении ресторана в базу данных: {e}")
     else:
-        print("Пропускаем ресторан, так как название не найдено")
+        logger.info("Пропускаем ресторан, так как название не найдено.")
 
     # Переходим на страницу с отзывами и ждём полной загрузки
+    logger.info(f"Переходим на страницу с отзывами: {reviews_url}")
     driver.get(reviews_url)
     sleep(5)
 
     # Вычисляем общее количество отзывов
-    total_count_element = WebDriverWait(driver, 5).until(
-        EC.visibility_of_element_located((By.CLASS_NAME, COUNT_REVIEWS_BLOCK))
-    )
-    total_count_text = total_count_element.text
-    total_count = int(total_count_text.split()[0])
-    print(f"Общее количество отзывов: {total_count}")
+    try:
+        total_count_element = WebDriverWait(driver, 5).until(
+            EC.visibility_of_element_located((By.CLASS_NAME, COUNT_REVIEWS_BLOCK))
+        )
+        total_count_text = total_count_element.text
+        total_count = int(total_count_text.split()[0])
+        logger.info(f"Общее количество отзывов: {total_count}")
+    except Exception as e:
+        logger.error(f"Ошибка при получении общего количества отзывов: {e}")
+        driver.quit()
+        return
+
     sleep(2)
 
     # Создаём текстовый документ для хранения и отправки отзывов юзеру
@@ -121,42 +150,53 @@ def ya_prim_coll(original_url):
                             (By.CSS_SELECTOR, RATING_ELEMENT)
                         )
                     ).get_attribute('content')
+                    logger.info(f"Рейтинг: {rating_value}")
                 except Exception as e:
-                    print(f"Ошибка при получении значения рейтинга: {e}")
+                    logger.error(f"Ошибка при получении значения рейтинга: {e}")
+                    rating_value = None
+
                 text = review.find_element(By.CLASS_NAME, TEXT_ELEMENT).text
+
                 # Сохранение отзыва в множество для уникальности
                 review_entry = (
                     review_date, author_name, rating_value, text)
                 unique_reviews.add(review_entry)
-                print(f'Уникальных отзывов: {len(unique_reviews)}')
+                logger.info(f'Уникальных отзывов: {len(unique_reviews)}')
 
             except Exception as e:
-                print(f"Ошибка при получении информации об отзыве: {e}")
+                logger.error(f"Ошибка при получении информации об отзыве: {e}")
 
         for i in range(0, 30):
             # Прокрутка страницы вниз для получения следующего отзыва
             actions.scroll_by_amount(0, 2000).perform()
             sleep(0.5)
 
-        # Сортировка отзывов по датe
-        sorted_reviews = sorted(
-            unique_reviews,
-            key=lambda x: datetime.strptime(x[0], DATE_FORMAT)
-        )
+    # Сортировка отзывов по дате
+    sorted_reviews = sorted(
+        unique_reviews,
+        key=lambda x: datetime.strptime(x[0], DATE_FORMAT)
+    )
 
-        # Определяем id ресторана для связи с отзывами
-        restaurant_data = read_some_restaurant_data(org_url=org_url)
-        restaurant_id = restaurant_data['id']
+    # Определяем id ресторана для связи с отзывами
+    restaurant_data = read_some_restaurant_data(org_url=org_url)
+    restaurant_id = restaurant_data['id']
+    logger.info(f"ID ресторана: {restaurant_id}")
 
-        # Запись уникальных отзывов в файл после завершения сбора данных
+    # Запись уникальных отзывов в базу данных
     for review in sorted_reviews:
-        # Запись отзыва в базу данных
         review_date, author_name, rating_value, text = review
         review_data = (
             restaurant_id, review_date, author_name, rating_value, text
         )
-        create_review(review_data)
+        try:
+            logger.info(f"Добавляем отзыв от {author_name} в базу данных...")
+            create_review(review_data)
+            logger.info(f"Отзыв от {author_name} успешно добавлен.")
+        except Exception as e:
+            logger.error(f"Ошибка при добавлении отзыва в базу данных: {e}")
 
     # Закрываем браузер
+    logger.info("Закрываем браузер.")
     driver.quit()
+
     return total_count
