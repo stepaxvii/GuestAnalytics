c38c80878137fb20829b81b1bfa1edae404699a2
stepaxvii <153988379+stepaxvii@users.noreply.github.com>
2025-01-12 09:29:46 +0300

    v/1.7

--- bot/periodically_tasks.py
+++ bot/periodically_tasks.py
@@ -2,7 +2,7 @@ import asyncio
 
 from aiogram import Bot
 
-from constants import TG_CHANNAL
+from constants import TG_CHANNAL, TG_ID
 from data_base.read_data import read_all_restaurant_data
 from utils.message_text import get_star_rating
 from yandex.yandex_check_new_reviews import matching_reviews
@@ -12,7 +12,7 @@ from yandex.yandex_check_new_reviews import matching_reviews
 async def check_new_reviews_periodically(bot: Bot):
     while True:
         # Пауза между проверками
-        await asyncio.sleep(200)
+        await asyncio.sleep(3600)
         print("Функция для проверки новых отзывов")
 
         # Получаем данные о ресторанах
@@ -41,5 +41,10 @@ async def check_new_reviews_periodically(bot: Bot):
                     # Отправляем сообщение в канал
                     await bot.send_message(TG_CHANNAL, message)
                     await asyncio.sleep(3)
+            else:
+                await bot.send_message(
+                    TG_ID,
+                    text=f"Новых отзывов для {rest_title} нет ("
+                )
 
         print("Проверка новых отзывов завершена.")
