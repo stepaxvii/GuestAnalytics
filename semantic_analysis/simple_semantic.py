729fb5d73d1f2a9ac62e59cab7d0c2c56e1a16c9
stepaxvii <153988379+stepaxvii@users.noreply.github.com>
2025-01-13 22:22:01 +0300

    v/2.1

--- semantic_analysis/simple_semantic.py
+++ semantic_analysis/simple_semantic.py
@@ -1,19 +1,20 @@
 import openai
-
 from os import getenv
 from dotenv import load_dotenv
 
 load_dotenv()
 
-OPEN_AI_API_KEY = getenv('OPEN_AI_API_KEY', default='')
+OPEN_AI_API_KEY = getenv('OPEN_AI_API_KEY')
+
+if not OPEN_AI_API_KEY:
+    raise ValueError("API Key is missing!")
 
 
 def simple_semantic(review_text):
     "Определение семантического настроения отдельного отзыва."
 
-    openai.api_key = OPEN_AI_API_KEY
+    openai.api_key = OPEN_AI_API_KEY  # Убедитесь, что ключ передан
 
-    # Промпт для OpenAI
     prompt = f"""Вы профессионал по анализу отзывов клиентов ресторана.
     Проведите семантический анализ и оцените, является ли следующий отзыв
      положительным (П), отрицательным (О) или нейтральным (Н).
@@ -24,13 +25,11 @@ def simple_semantic(review_text):
 
     Ваш ответ:"""
 
-    # Запрос к OpenAI API
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
-        messages=[
-            {"role": "user", "content": prompt}
-        ],
-        max_tokens=10,  # Ограничение на количество токенов
+        messages=[{"role": "user", "content": prompt}],
+        max_tokens=10,
         temperature=0.7
     )
+
     return response['choices'][0]['message']['content']
