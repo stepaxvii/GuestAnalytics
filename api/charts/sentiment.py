c1d9f48562430b0f321b5a509bcdf362f6a267e6
stepaxvii <153988379+stepaxvii@users.noreply.github.com>
2025-02-03 12:51:51 +0300

    v/4.96 edit charts

--- api/charts/sentiment.py
+++ api/charts/sentiment.py
@@ -72,6 +72,117 @@ sentiment_trend_bp = Blueprint('sentiment_trend', __name__)
 #     }), 200
 
 
+# @sentiment_trend_bp.route('/sentiment-trend', methods=['GET'])
+# def sentiment_trend():
+#     # Получаем user_id, который совпадает с restaurant_id
+#     user_id = request.args.get('user_id')
+#     if not user_id:
+#         return jsonify({
+#             "success": False,
+#             "data": None,
+#             "message": "Не указан user_id"
+#         }), 400
+
+#     # Получаем текущую дату
+#     current_date = datetime.now()
+
+#     # Генерируем список последних 4 месяцев в формате 'yyyy-mm'
+#     months = []
+#     for i in range(3, -1, -1):  # Сначала добавляем старые месяцы, начиная с 3
+#         # Используем relativedelta для точного вычитания месяцев
+#         prev_month_date = current_date - relativedelta(months=i)
+#         # Форматируем как 'yyyy-mm'
+#         prev_month = prev_month_date.strftime("%Y-%m")
+#         months.append(prev_month)
+
+#     # Пример запроса, чтобы получить отзывы по restaurant_id (user_id)
+#     reviews = session.query(YandexReview).filter(
+#         YandexReview.restaurant_id == user_id
+#     ).all()
+
+#     # Если нет отзывов, возвращаем пустой ответ
+#     if not reviews:
+#         return jsonify({
+#             "success": False,
+#             "data": None,
+#             "message": "Отзывы не найдены"
+#         }), 404
+
+#     # Создаем словарь для хранения количества  отзывов по месяцам
+#     sentiment_data = {
+#         "П": {month: 0 for month in months},
+#         "Н": {month: 0 for month in months},
+#         "О": {month: 0 for month in months}
+#     }
+
+#     # Функция для очистки и обработки значения поля semantic
+#     def get_sentiment(semantic_value):
+#         # Приводим к верхнему регистру и фильтруем только разрешенные символы
+#         clean_value = ''.join(
+#             [ch.upper() for ch in str(semantic_value) if ch.isalpha()]
+#         )
+
+#         # Если символ является 'О', 'Н' или 'П'
+#         if clean_value == 'П':
+#             return "П"
+#         elif clean_value == 'Н':
+#             return "Н"
+#         elif clean_value == 'О':
+#             return "О"
+#         # Для всех других случаев, считаем нейтральным
+#         else:
+#             return "Н"
+
+#     # Перебираем все отзывы
+#     for review in reviews:
+#         # Преобразуем строку в datetime (формат даты 'YYYY-MM-DD')
+#         review_date = datetime.strptime(review.created_at, "%Y-%m-%d")
+
+#         # Получаем месяц и год отзыва в формате 'yyyy-mm'
+#         review_month = review_date.strftime("%Y-%m")
+
+#         # Если месяц отзыва есть в нашем списке, считаем его для sentiment
+#         if review_month in sentiment_data:
+#             # Применяем функцию очистки и обработки
+#             sentiment = get_sentiment(review.semantic)
+
+#             # Увеличиваем соответствующий счетчик
+#             sentiment_data[sentiment][review_month] += 1
+
+#     # Переходим к подготовке данных для графика
+#     data = {
+#         "labels": [month_dict[month.split('-')[1]] for month in months],
+#         "datasets": [
+#             {
+#                 "label": "Положительные",
+#                 "data": [
+#                     sentiment_data["П"].get(month, 0) for month in months
+#                 ],
+#                 "borderColor": "#2ECC71"
+#             },
+#             {
+#                 "label": "Нейтральные",
+#                 "data": [
+#                     sentiment_data["Н"].get(month, 0) for month in months
+#                 ],
+#                 "borderColor": "#1E90FF"
+#             },
+#             {
+#                 "label": "Отрицательные",
+#                 "data": [
+#                     sentiment_data["О"].get(month, 0) for month in months
+#                 ],
+#                 "borderColor": "#FF6347"
+#             }
+#         ]
+#     }
+
+#     return jsonify({
+#         "success": True,
+#         "data": data,
+#         "message": "Данные по sentiment-trend получены"
+#     }), 200
+
 @sentiment_trend_bp.route('/sentiment-trend', methods=['GET'])
 def sentiment_trend():
     # Получаем user_id, который совпадает с restaurant_id
@@ -108,30 +219,12 @@ def sentiment_trend():
             "message": "Отзывы не найдены"
         }), 404
 
-    # Создаем словарь для хранения количества  отзывов по месяцам
+    # Создаем словарь для хранения количества положительных, нейтральных и отрицательных отзывов по месяцам
     sentiment_data = {
-        "П": {month: 0 for month in months},
-        "Н": {month: 0 for month in months},
-        "О": {month: 0 for month in months}
+        "Положительные": {month: 0 for month in months},
+        "Нейтральные": {month: 0 for month in months},
+        "Отрицательные": {month: 0 for month in months}
     }
-
-    # Функция для очистки и обработки значения поля semantic
-    def get_sentiment(semantic_value):
-        # Приводим к верхнему регистру и фильтруем только разрешенные символы
-        clean_value = ''.join(
-            [ch.upper() for ch in str(semantic_value) if ch.isalpha()]
-        )
-
-        # Если символ является 'О', 'Н' или 'П'
-        if clean_value == 'П':
-            return "П"
-        elif clean_value == 'Н':
-            return "Н"
-        elif clean_value == 'О':
-            return "О"
-        # Для всех других случаев, считаем нейтральным
-        else:
-            return "Н"
 
     # Перебираем все отзывы
     for review in reviews:
@@ -143,35 +236,37 @@ def sentiment_trend():
 
         # Если месяц отзыва есть в нашем списке, считаем его для sentiment
         if review_month in sentiment_data:
-            # Применяем функцию очистки и обработки
-            sentiment = get_sentiment(review.semantic)
+            # Получаем значение поля 'semantic' и проверяем его
+            sentiment = review.semantic
 
-            # Увеличиваем соответствующий счетчик
-            sentiment_data[sentiment][review_month] += 1
+            # Если semantic равно None (NULL), считаем отзыв нейтральным
+            if sentiment == "П":
+                sentiment_data["Положительные"][review_month] += 1
+            elif sentiment == "Н":
+                sentiment_data["Нейтральные"][review_month] += 1
+            elif sentiment == "О":
+                sentiment_data["Отрицательные"][review_month] += 1
+            else:
+                # Если значение semantic None, считаем отзыв нейтральным
+                sentiment_data["Нейтральные"][review_month] += 1
 
     # Переходим к подготовке данных для графика
     data = {
-        "labels": [month_dict[month.split('-')[1]] for month in months],
+        "labels": [month_dict[month.split('-')[1]] for month in months],  # Используем названия месяцев
         "datasets": [
             {
                 "label": "Положительные",
-                "data": [
-                    sentiment_data["П"].get(month, 0) for month in months
-                ],
+                "data": [sentiment_data["Положительные"].get(month, 0) for month in months],
                 "borderColor": "#2ECC71"
             },
             {
                 "label": "Нейтральные",
-                "data": [
-                    sentiment_data["Н"].get(month, 0) for month in months
-                ],
+                "data": [sentiment_data["Нейтральные"].get(month, 0) for month in months],
                 "borderColor": "#1E90FF"
             },
             {
                 "label": "Отрицательные",
-                "data": [
-                    sentiment_data["О"].get(month, 0) for month in months
-                ],
+                "data": [sentiment_data["Отрицательные"].get(month, 0) for month in months],
                 "borderColor": "#FF6347"
             }
         ]
