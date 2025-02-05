ab27eb673d27b4d2c6dbcb4249b5c6b46abc70b3
stepaxvii <153988379+stepaxvii@users.noreply.github.com>
2025-02-05 23:38:49 +0300

    v/4.99 front and back

--- api/restaurants/create_restaurant.py
+++ api/restaurants/create_restaurant.py
@@ -1,23 +1,21 @@
-# from flask import Blueprint, request
+from flask import Blueprint, request, jsonify
 
 
-# create_restaurant_bp = Blueprint('create_restaurant', __name__)
+create_restaurant_test_bp = Blueprint('create_restaurant_test', __name__)
 
 
-# @create_restaurant_bp.route('/create_restaurant', methods=['POST'])
-# def create_restaurant():
-#     rest_id = request.args.get('restaurant_id')
-#     rest_title = request.args.get('restaurant_name')
-#     rest_link = request.args.get('yandex_link')
-#     print(f'id = {rest_id}')
-#     print(f'title - {rest_title}')
-#     print(f'link - {rest_link}')
+@create_restaurant_test_bp.route('/create_restaurant', methods=['GET'])
+def create_restaurant_test():
+    rest_id = request.args.get('restaurant_id')
+    rest_title = request.args.get('restaurant_name')
+    rest_link = request.args.get('yandex_link')
+    print(f'id = {rest_id}')
+    print(f'title - {rest_title}')
+    print(f'link - {rest_link}')
 
 
 # # http://217.114.9.10:5000/api/create_restaurant
 
-
-from flask import Blueprint, request, jsonify
 
 create_restaurant_bp = Blueprint("create_restaurant", __name__)
 
