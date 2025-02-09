6ef2e893cf85670ceabd87a073a26e77a87c4794
stepaxvii <153988379+stepaxvii@users.noreply.github.com>
2025-02-10 00:31:03 +0300

    v/5.0 fix add reviews after yandex  primary collection

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import asyncio
 import logging
 import sys
-import threading
+from multiprocessing import Process
 
 from bot.bot_main import main as start_bot
 from api.app import start_flask
@@ -13,15 +13,19 @@ logging.basicConfig(
 )
 
 
-async def main():
-    # Запуск Flask сервера в отдельном потоке
-    flask_thread = threading.Thread(target=start_flask)
-    flask_thread.start()
+# Основная функция для запуска Flask и бота
+def main():
+    # Запуск Flask сервера в отдельном процессе
+    flask_process = Process(target=start_flask)
+    flask_process.start()
 
-    # Запуск бота в основном потоке
-    await start_bot()
+    # Запуск бота в основном потоке с использованием asyncio
+    asyncio.run(start_bot())
+
+    # Ждем завершения процесса Flask
+    flask_process.join()
 
 
 # Запуск
 if __name__ == "__main__":
-    asyncio.run(main())
+    main()
