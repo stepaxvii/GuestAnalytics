a7902fcc037da802fbefd315ba31705ff95df1bc
stepaxvii <153988379+stepaxvii@users.noreply.github.com>
2025-02-12 22:39:45 +0300

    v/5.3 edit restaurant data from api

--- api/app.py
+++ api/app.py
@@ -5,7 +5,7 @@ from api.charts import (
     reviews,
     sentiment
 )
-from api.restaurants import create_restaurant
+from api.restaurants import create_restaurant, edit_restaurant
 from api.reviews import check_reviews
 from api.db import create_tables  # Импортируем функцию для создания таблиц
 
@@ -21,13 +21,13 @@ app.register_blueprint(ratings.ratings_distribution_bp, url_prefix='/api')
 app.register_blueprint(ratings.ratings_trend_bp, url_prefix='/api')
 app.register_blueprint(sentiment.sentiment_distribution_bp, url_prefix='/api')
 app.register_blueprint(sentiment.sentiment_trend_bp, url_prefix='/api')
-app.register_blueprint(check_reviews.yandex_coll_bp, url_prefix='/api')
 app.register_blueprint(
     create_restaurant.create_restaurant_bp, url_prefix='/api'
 )
 app.register_blueprint(
-    check_reviews.yandex_prim_coll_bp, url_prefix='/api'
+    edit_restaurant.edit_restaurant_bp, url_prefix='/api'
 )
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
---
+++ api/restaurants/edit_restaurant.py
@@ -0,0 +1,60 @@
+import logging
+
+from flask import Blueprint, request, jsonify
+from api.db import session
+from data.data_main import Restaurant
+
+edit_restaurant_bp = Blueprint("edit_restaurant", __name__)
+
+
+@edit_restaurant_bp.route("/edit_restaurant", methods=["POST"])
+def edit_restaurant():
+    # Получаем данные из запроса
+    data = request.get_json(force=True)
+
+    rest_id = data.get("restaurant_id")
+    rest_title = data.get("restaurant_name")
+    rest_address = data.get("address")
+    tg_id = data.get("telegram_id")
+    action = data.get("action")
+
+    # Проверяем, если действие это обновление данных ресторана
+    if action == "update":
+        try:
+            # Ищем ресторан по ID
+            restaurant = session.query(Restaurant).filter_by(id=rest_id).first()
+            if not restaurant:
+                return jsonify(
+                    {"status": "error", "message": "Ресторан не найден."}
+                ), 404
+
+            # Обновляем данные ресторана (кроме id и yandex_link)
+            if rest_title:
+                restaurant.title = rest_title
+            if rest_address:
+                restaurant.address = rest_address
+            if tg_id:
+                restaurant.tg_channal = tg_id
+
+            # Сохраняем изменения в базе данных
+            session.commit()
+
+            return jsonify(
+                {"status": "ok", "message": "Данные успешно обновлены."}
+            ), 200
+        except Exception as e:
+            session.rollback()
+            logging.error(f"Произошла ошибка при обновлении данных: {e}")
+            return jsonify(
+                {
+                    "status": "error",
+                    "message": "Произошла ошибка при обновлении данных."
+                }
+            ), 500
+        finally:
+            session.close()
+
+    # Если действие не является 'update', возвращаем ошибку
+    return jsonify(
+        {"status": "error", "message": "Некорректное действие."}
+    ), 400
