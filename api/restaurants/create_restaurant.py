7d17f82ba4b8aea057d9a7b0f703c5e021135cf2
stepaxvii <153988379+stepaxvii@users.noreply.github.com>
2025-02-12 21:30:06 +0300

    v/5.3 edit create restaurant api for ya_prim_coll

--- api/restaurants/create_restaurant.py
+++ api/restaurants/create_restaurant.py
@@ -1,11 +1,21 @@
 import logging
+from threading import Thread
 
 from flask import Blueprint, request, jsonify
 from sqlalchemy.exc import IntegrityError
-from data.data_main import Restaurant, session
-
+from api.db import session
+from data.data_main import Restaurant
+from yandex.yandex_primary_collection_api import ya_prim_coll
 
 create_restaurant_bp = Blueprint("create_restaurant", __name__)
+
+
+# Функция для выполнения долгой задачи в фоне
+def run_yandex_check(yandex_link, restaurant_id):
+    try:
+        ya_prim_coll(original_url=yandex_link, rest_id=restaurant_id)
+    except Exception as e:
+        logging.error(f"Error occurred while calling ya_prim_coll: {e}")
 
 
 @create_restaurant_bp.route("/create_restaurant", methods=["POST"])
@@ -16,6 +26,7 @@ def create_restaurant():
     rest_id = data.get("restaurant_id")
     rest_title = data.get("restaurant_name")
     rest_link = data.get("yandex_link")
+    rest_address = data.get("address")
     tg_id = data.get("telegram_id")
     action = data.get("action")
 
@@ -27,15 +38,22 @@ def create_restaurant():
                 id=rest_id,
                 title=rest_title,
                 yandex_link=rest_link,
-                address="default address",
+                address=rest_address,
                 tg_channal=tg_id,
             )
             # Добавляем ресторан в сессию и сохраняем
             session.add(restaurant)
             session.commit()
+
+            # Запускаем выполнение функции в отдельном потоке
+            thread = Thread(
+                target=run_yandex_check, args=(rest_link, rest_id)
+            )
+            thread.start()
+
             return jsonify(
                 {"status": "ok", "message": "Restaurant created successfully."}
-                ), 200
+            ), 200
         except IntegrityError as e:
             session.rollback()
             logging.info(f"Ошибка уникальности: {e}")
