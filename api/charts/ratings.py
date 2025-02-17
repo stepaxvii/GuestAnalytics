02abe3afce900a2920a7a47a36f32afe1b054146
stepaxvii <153988379+stepaxvii@users.noreply.github.com>
2025-02-17 22:51:44 +0300

    v/5.74 test restaurant_id from args & process url before save

--- api/charts/ratings.py
+++ api/charts/ratings.py
@@ -127,27 +127,37 @@ def ratings_trend():
             "datasets": [
                 {
                     "label": "5 звёзд",
-                    "data": [ratings_count_by_month[month][5] for month in months],
+                    "data": [
+                        ratings_count_by_month[month][5] for month in months
+                    ],
                     "borderColor": "#2ECC71"
                 },
                 {
                     "label": "4 звезды",
-                    "data": [ratings_count_by_month[month][4] for month in months],
+                    "data": [
+                        ratings_count_by_month[month][4] for month in months
+                    ],
                     "borderColor": "#3498DB"
                 },
                 {
                     "label": "3 звезды",
-                    "data": [ratings_count_by_month[month][3] for month in months],
+                    "data": [
+                        ratings_count_by_month[month][3] for month in months
+                    ],
                     "borderColor": "#9B59B6"
                 },
                 {
                     "label": "2 звезды",
-                    "data": [ratings_count_by_month[month][2] for month in months],
+                    "data": [
+                        ratings_count_by_month[month][2] for month in months
+                    ],
                     "borderColor": "#F1C40F"
                 },
                 {
                     "label": "1 звезда",
-                    "data": [ratings_count_by_month[month][1] for month in months],
+                    "data": [
+                        ratings_count_by_month[month][1] for month in months
+                    ],
                     "borderColor": "#E74C3C"
                 }
             ]
--- utils/urls.py
+++ utils/urls.py
@@ -1,3 +1,48 @@
+import logging
+from time import sleep
+from os import getenv
+
+from dotenv import load_dotenv
+from selenium.webdriver import Firefox, FirefoxOptions
+from selenium.webdriver.firefox.service import Service
+
+# Настройка логирования
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+load_dotenv()
+
+DRIVER_PATH = getenv('DRIVER_PATH')
+TG_GROUP = getenv('TG_GROUP')
+
+
+def check_full_url_yandex(user_url):
+    """Функция проверки полной ссылки."""
+
+    logger.info(f"Формируем полный адрес URL по user_url: {user_url}")
+
+    options = FirefoxOptions()
+    options.add_argument('--headless')
+    options.add_argument('--no-sandbox')  # Для работы с Docker
+    options.add_argument('--disable-dev-shm-usage')  # Для работы с Docker
+    service = Service(DRIVER_PATH)
+
+    try:
+        logger.info("Запускаем Firefox WebDriver...")
+        driver = Firefox(service=service, options=options)
+        logger.info("WebDriver успешно запущен.")
+    except Exception as e:
+        logger.error(f"Ошибка при запуске WebDriver: {e}")
+        return
+
+    driver.get(user_url)
+    sleep(1)
+    full_org_url = driver.current_url
+    logger.info(f"Полный URL компании: {full_org_url}")
+
+    return full_org_url
+
+
 def process_url_yandex(original_url):
     """Функция форматирования валидного адреса для Яндекса."""
 
