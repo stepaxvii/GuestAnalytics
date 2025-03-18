c1d6af5c54bff978f588f484f9e8bfbe3c9c9e6c
stepaxvii <153988379+stepaxvii@users.noreply.github.com>
2025-03-18 18:24:35 +0300

    v/6.1 edit count insights

--- api/app.py
+++ api/app.py
@@ -1,5 +1,51 @@
-import logging
+# import logging
 
+# from flask import Flask
+# from api.charts import (
+#     dashboard,
+#     ratings,
+#     reviews,
+#     sentiment
+# )
+# from api.restaurants import (
+#     change_subscription,
+#     create_restaurant,
+#     edit_restaurant
+# )
+# # Импортируем функцию для создания таблиц
+# from api.db import create_tables
+
+# # Настройка логирования
+# logging.basicConfig(level=logging.DEBUG)
+
+# app = Flask(__name__)
+
+# # Создаем таблицы, если их еще нет
+# create_tables()
+
+# # Регистрируем Blueprint для каждого API
+# app.register_blueprint(dashboard.dashboard_bp, url_prefix='/api')
+# app.register_blueprint(reviews.total_reviews_bp, url_prefix='/api')
+# app.register_blueprint(reviews.trend_reviews_bp, url_prefix='/api')
+# app.register_blueprint(ratings.ratings_distribution_bp, url_prefix='/api')
+# app.register_blueprint(ratings.ratings_trend_bp, url_prefix='/api')
+# app.register_blueprint(sentiment.sentiment_distribution_bp, url_prefix='/api')
+# app.register_blueprint(sentiment.sentiment_trend_bp, url_prefix='/api')
+# app.register_blueprint(
+#     change_subscription.change_subscription_bp, url_prefix='/api'
+# )
+# app.register_blueprint(
+#     create_restaurant.create_restaurant_bp, url_prefix='/api'
+# )
+# app.register_blueprint(
+#     edit_restaurant.edit_restaurant_bp, url_prefix='/api'
+# )
+
+# app.logger.setLevel(logging.DEBUG)
+
+# if __name__ == '__main__':
+#     app.run(host='0.0.0.0', port=5000, debug=True)
+import logging
 from flask import Flask
 from api.charts import (
     dashboard,
@@ -16,7 +62,7 @@ from api.restaurants import (
 from api.db import create_tables
 
 # Настройка логирования
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.INFO)  # Устанавливаем уровень логирования INFO
 
 app = Flask(__name__)
 
@@ -40,6 +86,16 @@ app.register_blueprint(
 app.register_blueprint(
     edit_restaurant.edit_restaurant_bp, url_prefix='/api'
 )
+
+# Настройка логирования для Flask
+app.logger.setLevel(logging.INFO)
+
+
+# Пример логирования для Flask
+@app.route('/')
+def hello_world():
+    app.logger.info('Обработка запроса к корневому маршруту')
+    return 'Hello, World!'
 
 
 if __name__ == '__main__':
--- bot/bot_main.py
+++ bot/bot_main.py
@@ -1,3 +1,61 @@
+# import asyncio
+# import logging
+# import sys
+# from os import getenv
+
+# from aiogram import Bot, Dispatcher
+# from aiogram.client.default import DefaultBotProperties
+# from aiogram.enums import ParseMode
+# from dotenv import load_dotenv
+
+# from bot import periodically_tasks
+# from bot.handlers import insight, start, yandex, data_edit
+
+# load_dotenv()
+
+# # Из окружения извлекаем необходимые токены, ключи и переменные
+# TELEGRAM_TOKEN = getenv('TELEGRAM_TOKEN')
+
+
+# # Запуск планировщика внутри функции main
+# async def main():
+#     bot = Bot(
+#         token=TELEGRAM_TOKEN,
+#         default=DefaultBotProperties(parse_mode=ParseMode.HTML)
+#     )
+#     dp = Dispatcher()
+#     dp.include_routers(
+#         start.router,
+#         yandex.router,
+#         data_edit.router,
+#         insight.router
+#     )
+
+#     # Запуск фоновой задачи для проверки новых отзывов
+#     periodic_task = asyncio.create_task(
+#         periodically_tasks.check_new_reviews_periodically(bot)
+#     )
+#     another_periodic_task = asyncio.create_task(
+#         periodically_tasks.check_new_insight_periodically(bot)
+#     )
+
+#     try:
+#         await bot.delete_webhook(drop_pending_updates=True)
+#         await dp.start_polling(bot)
+#         # Дожидаемся завершения фоновых задач
+#         await periodic_task
+#         await another_periodic_task
+#     except Exception as error:
+#         logging.error(f"Произошла ошибка: {error}")
+
+
+# if __name__ == "__main__":
+#     logging.basicConfig(
+#         level=logging.INFO,
+#         stream=sys.stdout,
+#     )
+#     asyncio.run(main())
+
 import asyncio
 import logging
 import sys
@@ -15,6 +73,12 @@ load_dotenv()
 
 # Из окружения извлекаем необходимые токены, ключи и переменные
 TELEGRAM_TOKEN = getenv('TELEGRAM_TOKEN')
+
+# Настройка логирования
+logging.basicConfig(
+    level=logging.INFO,  # Устанавливаем уровень логирования INFO
+    stream=sys.stdout,    # Выводим логи в стандартный вывод
+)
 
 
 # Запуск планировщика внутри функции main
@@ -48,10 +112,7 @@ async def main():
     except Exception as error:
         logging.error(f"Произошла ошибка: {error}")
 
-
 if __name__ == "__main__":
-    logging.basicConfig(
-        level=logging.INFO,
-        stream=sys.stdout,
-    )
+    # Логирование в main, если нужно вывести дополнительные логи
+    logging.info('Запуск Telegram-бота...')
     asyncio.run(main())
